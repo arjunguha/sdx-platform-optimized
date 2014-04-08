791c4a506f6285e678631eaec364e829b46b08ee
Arpit <glex.qsd@gmail.com>
2014-04-08 14:55:21 -0400

    missing files from original repo

--- pyretic/sdx/bgp/peer.py
+++ pyretic/sdx/bgp/peer.py
@@ -5,34 +5,45 @@
 import json
 import sqlite3
 from rib import rib
-from decision_process import decision_process
 
 class peer():
     
-    def __init__(self,ip):
+    def __init__(self,ips):
         
-        self.ip = ip
-        self.rib = {"input": rib("input"),
-                    "local": rib("local")}
+        self.ips = ips
+        self.rib = {"input": rib("-".join(ips),"input"),
+                    "local": rib("-".join(ips),"local")}
          
-    def update(self,route,queue):
+    def update(self,route):
 
         origin = None
         as_path = None
         med = None
         atomic_aggregate = None
+        route_list = []
         
         if ('neighbor' in route):
             if ('ip' in route['neighbor']):
-                # Only add to the RIB if it's from a participant who other than myself.
-                if (route['neighbor']['ip'] != self.ip):
+                # Only add to the RIB if it's for myself.
+                if (route['neighbor']['ip'] in self.ips):
+                    if ('state' in route['neighbor'] and route['neighbor']['state']=='down'):
+                        #FIXME: Glue logic
+                        routes = self.rib['input'].get_all()
+                        
+                        for route_item in routes:
+                            self.rib['local'].delete(route_item['prefix'])
+                        self.rib['local'].commit()
+                        
+                        self.rib["input"].delete_all()
+                        self.rib["input"].commit()
+                        
                     if ('update' in route['neighbor']):
                         if ('attribute' in route['neighbor']['update']):
                             attribute = route['neighbor']['update']['attribute']
-                            origin = attribute['origin']
-                            as_path = attribute['as-path']
-                            med = attribute['med']
-                            atomic_aggregate = attribute['atomic-aggregate']
+                            origin = attribute['origin'] if 'origin' in attribute else ''
+                            as_path = attribute['as-path'] if 'as-path' in attribute else ''
+                            med = attribute['med'] if 'med' in attribute else ''
+                            atomic_aggregate = attribute['atomic-aggregate'] if 'atomic-aggregate' in attribute else ''
                             
                         if ('announce' in route['neighbor']['update']):
                             announce = route['neighbor']['update']['announce']
@@ -40,17 +51,13 @@ class peer():
                                 for prefix in announce['ipv4 unicast'].keys():
                                     self.rib["input"][prefix] = (announce['ipv4 unicast'][prefix]['next-hop'],
                                                                  origin,
-                                                                 ' '.join(map(str,as_path)).replace('[','').replace(']',''),
+                                                                 ' '.join(map(str,as_path)).replace('[','').replace(']','').replace(',',''),
                                                                  med,
                                                                  atomic_aggregate)
                                     self.rib["input"].commit()
-                                    best_route = decision_process(self.rib["input"],prefix)
+                                    announce_route = self.rib["input"][prefix]
                                     
-                                    if (best_route is not None):
-                                        self.rib["local"].delete(prefix)
-                                        self.rib["local"][prefix] = best_route
-                                        self.rib["local"].commit()
-                                        queue.put(self.announce_route(best_route))
+                                    route_list.append({'announce': announce_route})
 
                         elif ('withdraw' in route['neighbor']['update']):
                             withdraw = route['neighbor']['update']['withdraw']
@@ -59,17 +66,22 @@ class peer():
                                     deleted_route = self.rib["input"][prefix]
                                     self.rib["input"].delete(prefix)
                                     self.rib["input"].commit()
-                                    self.rib["local"].delete(prefix)
-                                    self.rib["local"].commit()
-                                    queue.put(self.withdraw_route(deleted_route))   
+                                    
+                                    route_list.append({'withdraw': deleted_route})
                                     # TODO: clarify how the route withdrawal will work i.e., do we have to run the decision process again
-
+                                    
         elif ('notification' in route):
+            
+            #return
+            
             if ('shutdown' == route['notification']):
                 self.rib["input"].delete_all()
                 self.rib["input"].commit()
                 self.rib["local"].delete_all()
                 self.rib["local"].commit()
+                # TODO: send shutdown notification to participants 
+        
+        return route_list
     
     def add_route(self,rib_name,prefix,attributes):
         self.rib[rib_name][prefix] = attributes
@@ -82,6 +94,10 @@ class peer():
     def get_route(self,rib_name,prefix):
         
         return self.rib[rib_name][prefix]
+    
+    def get_routes(self,rib_name,prefix):
+        
+        return self.rib[rib_name].get_all(prefix)
     
     def get_all_routes(self, rib_name):
         
@@ -101,43 +117,65 @@ class peer():
         
         return self.rib[rib_name].filter(item,value)
     
-    def announce_route(self,route):
+def announce_route(route,next_hop=None):
        
-        if (isinstance(route,tuple) or isinstance(route,list)):
-            value = "announce route " + route[0] + " next-hop " + route[1]
-            
-            if ('as_path' in route):
-                value += " as-path [ " + route[3] + " ]"
-                
-            return value
-        elif (isinstance(route,dict) or isinstance(route,sqlite3.Row)):
-            value = "announce route " + route['prefix'] + " next-hop " + route['next_hop'] 
-        
-            if ('as_path' in route.keys()):
-                value += " as-path [ " + route['as_path'] + " ]"
-            
-            return value
+    if (isinstance(route,tuple) or isinstance(route,list)):
+        if next_hop:
+            inext_hop = next_hop
         else:
-            return None
+            inext_hop = route[1]
+            
+        value = "announce route " + route[0] + " next-hop " + inext_hop
+            
+        if ('as_path' in route):
+            value += " as-path [ " + route[3] + " ]"
+                
+        return value
+        
+    elif (isinstance(route,dict) or isinstance(route,sqlite3.Row)):
+        if next_hop:
+            inext_hop = next_hop
+        else:
+            inext_hop = route['next_hop'] 
+            
+        value = "announce route " + route['prefix'] + " next-hop " + inext_hop
+        
+        if ('as_path' in route.keys()):
+            value += " as-path [ " + route['as_path'] + " ]"
+            
+        return value
+    else:
+        return None
     
-    def withdraw_route(self,route):
+def withdraw_route(route,next_hop=None):
         
-        if (isinstance(route,tuple) or isinstance(route,list)):
-            value = "withdraw route " + route[0] + " next-hop " + route[1]
-            
-            if ('as-path' in route):
-                value += " as-path [ " + route[3] + " ]"
-                
-            return value
-        elif (isinstance(route,dict) or isinstance(route,sqlite3.Row)):
-            value = "withdraw route " + route['prefix'] + " next-hop " + route['next_hop'] 
-        
-            if ('as-path' in route):
-                value += " as-path [ " + route['as_path'] + " ]"
-            
-            return value
+    if (isinstance(route,tuple) or isinstance(route,list)):
+        if next_hop:
+            inext_hop = next_hop
         else:
-            return None
+            inext_hop = route[1]
+                
+        value = "withdraw route " + route[0] + " next-hop " + inext_hop
+            
+        if ('as-path' in route):
+            value += " as-path [ " + route[3] + " ]"
+                
+        return value
+    
+    elif (isinstance(route,dict) or isinstance(route,sqlite3.Row)):
+        if next_hop:
+            inext_hop = next_hop
+        else:
+            inext_hop = route['next_hop'] 
+            
+        value = "withdraw route " + route['prefix'] + " next-hop " + inext_hop
+        
+        if ('as_path' in route.keys()):
+            value += " as-path [ " + route['as_path'] + " ]"
+            
+        return value
+    else:
+        return None
 
 ''' main '''    
 if __name__ == '__main__':
--- pyretic/sdx/bgp/route_server.py
+++ pyretic/sdx/bgp/route_server.py
@@ -6,44 +6,84 @@
 import json
 from peer import peer as Peer
 from server import server as Server
-from pyretic.sdx.lib.core import sdx_update_route
+#from decision_process_simple import decision_process_simple as decision_process
+from decision_process import decision_process
+
+from pyretic.sdx.utils import get_participants_ports_list
+from pyretic.sdx.lib.bgp_interface import *
+from pyretic.sdx.bgp.peer import *
 
 class route_server():
     
-    def __init__(self, peers_list):
+    def __init__(self,event_queue,ready_queue,sdx):
         
-        self.peers = {}
-        self.peers_list = peers_list
-        
-        self.server = None
-        
-    def start(self,event_queue,sdx):
-        
-        # TODO: resolve sql-lite multi-threading issue - MS
-        for peer_item in self.peers_list:
-            if (peer_item not in self.peers.keys()):
-                self.peers[peer_item] = Peer(peer_item)
-                
+        self.event_queue = event_queue
+        self.ready_queue = ready_queue
         self.server = Server()
+        
+        self.sdx = sdx
+        self.sdx.server = self.server
+        
+        participants_ports_list = get_participants_ports_list(sdx.participants)
+        
+        ''' Create and assign participant RIBs '''
+        for participant_name in sdx.participants:
+            self.sdx.participants[participant_name].rs_client = Peer(participants_ports_list[participant_name])
+        
+    def start(self):
+        
+        self.server.start()
     
         while True:
-            try:
-                route = self.server.receiver_queue.get()
-                route = json.loads(route)
+            route = self.server.receiver_queue.get()
+            route = json.loads(route)
                 
-                updated_route = sdx_update_route(sdx,route,event_queue)
+            # TODO: check the RIB update flow with others ...
+            # At the moment, I am updating the RIB and will attach next-hop to the announcement at the end
                 
-                for peer in self.peers:
-                    self.peers[peer].update(updated_route,self.server.sender_queue)
-            except:
-                print 'route_sever_error: thread ended'
-                break
-    
+            updates = []
+            route_list = None
+            
+            # Update RIBs
+            for participant_name in self.sdx.participants:
+                route_list = self.sdx.participants[participant_name].rs_client.update(route)
+                if route_list:
+                    break
+                
+            # Best Path Selection algorithm
+            for route_item in route_list:
+                updates.extend(decision_process(self.sdx.participants,route_item))
+            
+            # Check for withdraw routes
+            for route in route_list:
+                if (route is None):
+                    continue
+                elif 'withdraw' in route:
+                    for VNH in self.sdx.VNH_2_pfx:
+                        if(route['withdraw']['prefix'] in list(self.sdx.VNH_2_pfx[VNH])):
+                            self.server.sender_queue.put(withdraw_route(route['withdraw'],self.sdx.VNH_2_IP[VNH]))
+                            break
+            
+            # Trigger policy updates
+            bgp_trigger_update(self.event_queue,self.ready_queue)
+           
+            # Check for announced routes         
+            #if (route_list):
+            # TODO: need to correct this glue logic
+            if hasattr(self.sdx, 'VNH_2_pfx'):
+                for VNH in self.sdx.VNH_2_pfx:
+                    for prefix in list(self.sdx.VNH_2_pfx[VNH]):
+                        for paticipant_name in self.sdx.participants:
+                            route = self.sdx.participants[paticipant_name].rs_client.get_route('local',prefix)
+                            if route:
+                                self.server.sender_queue.put(announce_route(route,self.sdx.VNH_2_IP[VNH]))
+                                break
+
 ''' main '''    
 if __name__ == '__main__':
     
-    peers_list = ['172.0.0.1', '172.0.0.11', '172.0.0.21', '172.0.0.22']
+    participants_list = {'A': ['172.0.0.1', '172.0.0.11'], 'B': ['172.0.0.21', '172.0.0.22']}
     
-    my_rs = route_server(peers_list)
+    my_rs = route_server(participants_list)
     my_rs.start(None,None)
     
